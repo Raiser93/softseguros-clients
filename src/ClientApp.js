3637829c7b50a0f6f122cc1ce6c2536e0a9f04ca
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-08 17:11:38 -0500

    Se implementa Redux a la app

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
 import { AppRouter } from './router/AppRouter';
+
 import { Container, Grid } from '@material-ui/core';
 import './style.css';
 
 export const ClientApp = () => {
     return (
-        <div className="main">
-            <Container>
-                <Grid container direction="row" justify="center" alignItems="center" className="grid-container-client">
-                    <AppRouter />
-                </Grid>
-            </Container>
-        </div>
+        <Provider store={store}>
+            <div className="main">
+                <Container>
+                    <Grid container direction="row" justify="center" alignItems="center" className="grid-container-client">
+                        <AppRouter />
+                    </Grid>
+                </Container>
+            </div>
+        </Provider>
     )
 }
