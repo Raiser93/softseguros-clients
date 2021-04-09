0d682462fd63f52ddb4bfacf00fb7ac06c7447c4
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-09 16:06:18 -0500

    Se genera actions para abrir y cerrar el backdrop

---
+++ src/actions/ui.js
@@ -0,0 +1,9 @@
+import { types } from "../types/types";
+
+export const uiOpenBackDrop = () => ({
+    type: types.uiOpenBackDrop,
+});
+
+export const uiCloseBackDrop = () => ({
+    type: types.uiCloseBackDrop,
+});
